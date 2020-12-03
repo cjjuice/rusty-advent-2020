d668e58f9742226ca2bf1aeb506ebb048e6e06aa
C.J. Bordeleau <cj@cj.gg>
2020-12-03 15:30:53 -0500

    complete part 2 of day 1

--- src/day_one/mod.rs
+++ src/day_one/mod.rs
@@ -1,16 +1,23 @@
 use std::fs;
 
 pub fn run() {
-    let input = fs::read_to_string("inputs/1.txt")
+    let input: String = fs::read_to_string("inputs/1.txt")
             .expect("Something went wrong reading the file");
 
-    'outer: for line1 in input.lines() {
-        for line2 in input.lines() {
+    two_numbers(&input);
+    three_numbers(&input);
+}
+
+fn two_numbers(content: &str) {
+    'outer: for line1 in content.lines() {
+        for line2 in content.lines() {
             let line1_int: i32 = line1.parse().unwrap();
             let line2_int: i32 = line2.parse().unwrap();
 
-           if evaluate_pair(line1_int, line2_int) {
-               println!("The answer is: {}", (line1_int * line2_int));
+            let arr: [i32;3] = [line1_int, line2_int, 0];
+
+           if evaluate_numbers(arr) {
+               println!("The answer for part 1 (two numbers) is: {}", (line1_int * line2_int));
 
                break 'outer;
            }
@@ -18,8 +25,28 @@ pub fn run() {
    }
 }
 
-fn evaluate_pair(line1: i32, line2: i32) -> bool {
-    let sum: i32 = line1 + line2;
+fn three_numbers(content: &str) {
+    'outer: for line1 in content.lines() {
+        for line2 in content.lines() {
+            for line3 in content.lines() {
+                let line1_int: i32 = line1.parse().unwrap();
+                let line2_int: i32 = line2.parse().unwrap();
+                let line3_int: i32 = line3.parse().unwrap();
+
+                let arr: [i32;3] = [line1_int, line2_int, line3_int];
+
+                if evaluate_numbers(arr) {
+                    println!("The answer for part 2 (three numbers) is: {}", (line1_int * line2_int * line3_int));
+
+                    break 'outer;
+                }
+            }
+       }
+   }
+}
+
+fn evaluate_numbers(numbers: [i32;3]) -> bool {
+    let sum: i32 = numbers.iter().sum();
 
     if sum == 2020 {
         true
