5d17920a13b4e6466acb224f30087b4ea2531190
C.J. Bordeleau <cj@cj.gg>
2020-12-07 10:22:00 -0500

    add part 2 of day 2

--- src/day_two/mod.rs
+++ src/day_two/mod.rs
@@ -1,22 +1,29 @@
 pub fn run() {
     let input = include_str!("../inputs/2.txt");
 
-    check_passwords(input);
+    check_passwords(input, 1);
+    check_passwords(input, 2);
 }
 
-fn check_passwords(content: &'static str) {
+fn check_passwords(content: &'static str, part: i8) {
     let mut valid_password_count = 0;
 
     for line in content.lines() {
-        if valid_password(line) {
-            valid_password_count += 1;
+        if part == 1 {
+          if valid_password_part_one(line) {
+              valid_password_count += 1;
+          }
+        } else {
+          if valid_password_part_two(line) {
+              valid_password_count += 1;
+          }
         }
     }
 
-    println!("Number of valid passwords: {}", valid_password_count);
+    println!("Number of valid passwords from part {}: {}", part, valid_password_count);
 }
 
-fn valid_password(password_line: &'static str) -> bool {
+fn valid_password_part_one(password_line: &'static str) -> bool {
     let vec:Vec<&str> = password_line.split_whitespace().collect();
     let policy_vec:Vec<&str> = vec[0].split("-").collect();
 
@@ -34,6 +41,28 @@ fn valid_password(password_line: &'static str) -> bool {
     }
 
     if occurences_count >= char_min && occurences_count <= char_max {
+        true
+    } else {
+        false
+    }
+}
+
+
+fn valid_password_part_two(password_line: &'static str) -> bool {
+    let vec:Vec<&str> = password_line.split_whitespace().collect();
+    let policy_vec:Vec<&str> = vec[0].split("-").collect();
+
+    let first_index: usize = policy_vec[0].parse().unwrap();
+    let second_index: usize = policy_vec[1].parse().unwrap();
+    let char_indicator = vec[1].chars().next().unwrap();
+    let password = vec[2];
+
+    let first_index_char = password.chars().nth(first_index - 1).unwrap();
+    let second_index_char = password.chars().nth(second_index - 1).unwrap();
+
+    if first_index_char == char_indicator && second_index_char != char_indicator {
+        true
+    } else if first_index_char != char_indicator && second_index_char == char_indicator {
         true
     } else {
         false
